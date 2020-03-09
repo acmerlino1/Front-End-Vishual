6bd2ef50d11413d2f6430205e389b3f0c7d18d89
Vish Mayer <vish.mayer@gmail.com>
2020-03-09 16:43:38 +0000

    refactor

--- src/AudioFeatures.js
+++ src/AudioFeatures.js
@@ -31,8 +31,8 @@ class AudioFeatures extends Component {
   render() {
 
     const keyInteger = {0: 'C', 1: 'C♯/D♭', 2: 'D', 3: 'D♯/E♭', 4: 'E', 5: 'F', 6: 'F♯/G♭', 7: 'G', 8: 'G♯/A♭', 9: 'A', 10: 'A♯/B♭', 11: 'B'}
-    const getDancelevel = {0: 'low-danceability', 0.1: 'low-danceability', 0.2: 'low-danceability', 0.3: 'low-danceability', 0.4: 'mid-danceability', 0.5: 'mid-danceability', 0.6: 'mid-danceability', 0.7: 'high-danceability', 0.8: 'high-danceability', 0.9: 'high-danceability', 1.0: 'high-danceability'}
-    const getEnergylevel = {0: 'Low Energy', 0.1: 'Low Energy', 0.2: 'Low Energy', 0.3: 'Low Energy', 0.4: 'Medium Energy', 0.5: 'Medium Energy', 0.6: 'Medium Energy', 0.7: 'High Energy', 0.8: 'High Energy', 0.9: 'High Energy', 1.0: 'High Energy'}
+    const getDancelevel = {0: 'Low', 0.1: 'Low', 0.2: 'Low', 0.3: 'Low', 0.4: 'Medium', 0.5: 'Medium', 0.6: 'Medium', 0.7: 'High', 0.8: 'High', 0.9: 'High', 1.0: 'High'}
+    const getEnergylevel = {0: 'Low', 0.1: 'Low', 0.2: 'Low', 0.3: 'Low', 0.4: 'Medium', 0.5: 'Medium', 0.6: 'Medium', 0.7: 'High', 0.8: 'High', 0.9: 'High', 1.0: 'High'}
     const getValencelevel = {0: 'Melancholy', 0.1: 'Melancholy', 0.2: 'Melancholy', 0.3: 'Melancholy', 0.4: 'Neutral', 0.5: 'Neutral', 0.6: 'Neutral', 0.7: 'Positive', 0.8: 'Positive', 0.9: 'Positive', 1.0: 'Positive'}
     const getLivenessLevel = {0: 'Studio', 0.1: 'Studio', 0.2: 'Studio', 0.3: 'Studio', 0.4: 'Live Recording', 0.5: 'Live Recording', 0.6: 'Live Recording', 0.7: 'Live Recording', 0.8: 'Live Recording', 0.9: 'Live Recording', 1.0: 'Live Recording'}
 
@@ -45,6 +45,7 @@ class AudioFeatures extends Component {
         return "Loud"
       }
     }
+
 
     function setInstrumentalnessValue(getInstrumentalness) {
       if (getInstrumentalness > .6) {
@@ -66,11 +67,11 @@ class AudioFeatures extends Component {
     
     return (
     <div className="AudioFeatures">
-        <div> Selected song danceability: { getDancelevel[parseFloat(this.state.songFeatures.danceability).toFixed(1)] }</div>
+        <div> Selected song danceability: { getDancelevel[parseFloat(this.state.songFeatures.danceability).toFixed(1)] } Danceability</div>
         <div> Selected song key: { keyInteger[this.state.songFeatures.key] }</div>
         <div> Selected song time signature: { this.state.songFeatures.time_signature }</div>
         <div> Selected song duration: { millisToMinutesAndSeconds(this.state.songFeatures.duration_ms) }</div>
-        <div> Selected song energy: { getEnergylevel[parseFloat(this.state.songFeatures.energy).toFixed(1)] }</div>
+        <div> Selected song energy: { getEnergylevel[parseFloat(this.state.songFeatures.energy).toFixed(1)] } Energy</div>
         <div> Selected song tempo: { parseFloat(this.state.songFeatures.tempo).toFixed(0) } BPM</div>
         <div> Selected song acousticness: { this.state.songFeatures.acousticness }</div>
         <div> Selected song instrumentalness: { setInstrumentalnessValue(this.state.songFeatures.instrumentalness) }</div>
