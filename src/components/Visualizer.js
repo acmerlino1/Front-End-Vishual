df33e14a5da392e1418c8df06cf3b2c9da2accfb
AliciaMerlino <acmerlino1@gmail.com>
2020-03-10 23:01:32 +0000

    Circles have random size and speed

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import Konva from "konva";
-import { render } from "react-dom";
-import { Stage, Layer, Circle, Line, Text, Rect } from "react-konva";
+import { Stage, Layer, Circle, Rect } from "react-konva";
 import "./App.css";
 
 class Visualizer extends Component {
@@ -17,14 +15,30 @@ class Visualizer extends Component {
       songInfo: this.props.songFeatures
     });
     for (var i = 0; i < 20; i++) {
-      this.state.circleArray.push({
-        x: Math.random() * window.innerWidth,
-        y: Math.random() * window.innerHeight,
-        red: Math.random() * 255,
-        green: Math.random() * 255,
-        blue: Math.random() * 255
-      });
+      this.state.circleArray.push(
+        new movingCircle(
+          Math.random() * window.innerWidth,
+          Math.random() * window.innerHeight,
+          (Math.random() - 0.5) * 9,
+          (Math.random() - 0.5) * 9,
+          Math.random() * 30 + 20,
+          Math.random() * 255,
+          Math.random() * 255,
+          Math.random() * 255
+        )
+      );
     }
+
+    setInterval(() => {
+      let circleArray = this.state.circleArray;
+      for (var i = 0; i < circleArray.length; i++) {
+        let c = circleArray[i];
+        c.update();
+      }
+      this.setState({
+        circleArray: circleArray
+      });
+    }, 10);
   }
 
   render() {
@@ -33,13 +47,13 @@ class Visualizer extends Component {
         width={window.innerWidth}
         height={window.innerHeight}
         style={{
-          zIndex: 0
+          zIndex: -1
         }}
       >
         <Layer>
           {this.state.circleArray.map(c => {
-            const fill = `rgb(${c.red}, ${c.green}, ${c.blue})`;
-            return <Circle x={c.x} y={c.y} radius={50} fill={fill} />;
+            const fill = `rgb(${c.r}, ${c.g}, ${c.b})`;
+            return <Circle x={c.x} y={c.y} radius={c.radius} fill={fill} />;
           })}
         </Layer>
       </Stage>
@@ -48,3 +62,25 @@ class Visualizer extends Component {
 }
 
 export default Visualizer;
+
+function movingCircle(x, y, dx, dy, radius, r, g, b) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.r = r;
+  this.g = g;
+  this.b = b;
+
+  this.update = function() {
+    if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+  };
+}
