26adbda304ad13359f2b10d6aaaf8eac90d07df9
AliciaMerlino <acmerlino1@gmail.com>
2020-03-10 23:48:35 +0000

    Add kaleidoscope filter

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,26 +1,20 @@
 import React, { Component } from "react";
+import Konva from "konva";
 import { Stage, Layer, Circle, Rect } from "react-konva";
 import "./App.css";
 
 class Visualizer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      circleArray: []
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      songInfo: this.props.songFeatures
-    });
+    var circleArray = [];
     for (var i = 0; i < 20; i++) {
-      this.state.circleArray.push(
+      circleArray.push(
         new movingCircle(
+          i,
           Math.random() * window.innerWidth,
           Math.random() * window.innerHeight,
-          (Math.random() - 0.5) * 9,
-          (Math.random() - 0.5) * 9,
+          (Math.random() - 0.5) * 50,
+          (Math.random() - 0.5) * 50,
           Math.random() * 30 + 20,
           Math.random() * 255,
           Math.random() * 255,
@@ -28,6 +22,16 @@ class Visualizer extends Component {
         )
       );
     }
+    this.state = {
+      songInfo: this.props.songFeatures,
+      circleArray: circleArray
+    };
+
+    this.circles = {};
+  }
+
+  componentDidMount() {
+    this.applyCache();
 
     setInterval(() => {
       let circleArray = this.state.circleArray;
@@ -38,7 +42,16 @@ class Visualizer extends Component {
       this.setState({
         circleArray: circleArray
       });
-    }, 10);
+    }, 50);
+  }
+
+  applyCache() {
+    var values = Object.values(this.circles);
+    for (var i = 0; i < values.length; i++) {
+      var c = values[i];
+      c.cache();
+      c.getLayer().batchDraw();
+    }
   }
 
   render() {
@@ -53,7 +66,19 @@ class Visualizer extends Component {
         <Layer>
           {this.state.circleArray.map(c => {
             const fill = `rgb(${c.r}, ${c.g}, ${c.b})`;
-            return <Circle x={c.x} y={c.y} radius={c.radius} fill={fill} />;
+            return (
+              <Circle
+                filters={[Konva.Filters.Noise]}
+                noise={1}
+                x={c.x}
+                y={c.y}
+                radius={c.radius}
+                fill={fill}
+                ref={node => {
+                  this.circles[c.id] = node;
+                }}
+              />
+            );
           })}
         </Layer>
       </Stage>
@@ -63,7 +88,8 @@ class Visualizer extends Component {
 
 export default Visualizer;
 
-function movingCircle(x, y, dx, dy, radius, r, g, b) {
+function movingCircle(id, x, y, dx, dy, radius, r, g, b) {
+  this.id = id;
   this.x = x;
   this.y = y;
   this.dx = dx;
